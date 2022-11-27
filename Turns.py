0da22b75aa8226df9dcfcb2d8ed8e7c37f39f1ef
Wenxi Chu <82306115+wenxichu@users.noreply.github.com>
2022-11-27 13:38:09 -0500

    Includes Bonus Match

--- Turns.py
+++ Turns.py
@@ -3,6 +3,56 @@ from Moves import CharStats, OppStats
 from Actions import Action, EnemyAction
 import time
 
+games_played = 0
+
+
+def prep_match():
+    fighter = Fighter()
+    enemy = Enemy()
+    # Get Fighter
+    if games_played == 0:
+        fighter.set_fighter()
+        enemy.enemy_profile()
+    else:
+        bonus_fighter()
+        bonus_enemy()
+    # Get Stats
+    print(Stats().check_stats())
+
+
+def bonus_fighter():
+    Fighter()._attack = 2
+    Fighter()._defense = 3
+    Fighter()._special = "Avoid Eye Contact"
+
+    attack = 2
+    defense = 3
+    special = "Avoid Eye Contact"
+
+    Fighter.char_stats.update({"Profile": "Virgin", "Attack": attack, "Defense": defense, "Special": special})
+    print("\n" + '\033[1mVirgin\033[0m')
+    print(f"Attack: {attack}\nDefense: {defense}\nSpecial: {special}\n")
+    Fighter.char_copy = dict(Fighter.char_stats)
+
+
+def bonus_enemy():
+    Enemy()._attack = 3
+    Enemy()._defense = 2
+    Enemy()._special = "Rant About Society"
+
+    enemy_attack = 3
+    enemy_defense = 2
+    enemy_special = "Rant About Society"
+
+    Enemy.opp_stats.update({"Profile": "Incel", "Attack": enemy_attack, "Defense": enemy_defense,
+                            "Special": enemy_special})
+    Enemy.opp_copy = dict(Enemy.opp_stats)
+    time.sleep(Enemy.countdown)
+
+    print("Your opponent is the\033[1m Incel\033[0m.\n")
+    print(f"Attack: {enemy_attack}\nDefense: {enemy_defense}\nSpecial: {enemy_special}")
+    print("\nPreparing for battle...\n")
+
 
 class Health:
 
@@ -26,9 +76,13 @@ class Round(CharStats, OppStats):
     def __init__(self):
         super().__init__()
         self.num_turns = 0
+        prep_match()
+        print(f"Health: 25/25    Charge: 0/4    Special: {Fighter.char_stats['Special']}")
         self.run_game()
         print(f"\nThis battle lasted a total of {self.num_turns} rounds.")
         self.trophies()
+        global games_played
+        games_played = 1
         # Reset Values
         CharStats.char_hp, CharStats.char_cg = (25, 0)
         OppStats.opp_hp, OppStats.opp_cg = (25, 0)
@@ -37,14 +91,6 @@ class Round(CharStats, OppStats):
         return hit_points <= 0
 
     def run_game(self):
-        fighter = Fighter()
-        enemy = Enemy()
-        # Get Fighter
-        fighter.set_fighter()
-        enemy.enemy_profile()
-        # Get Stats
-        print(Stats().check_stats())
-        print(f"Health: 25/25    Charge: 0/4    Special: {fighter.special}")
         # Turn Order
         while CharStats.char_hp > 0 and OppStats.opp_hp > 0:
             Health().check_limit()
@@ -63,7 +109,7 @@ class Round(CharStats, OppStats):
             print("The judges will now decide on your performance.")
         # Lose
         elif self.__le__(CharStats.char_hp):
-            print(f"\n{Enemy.opponent_stats['Profile']} has won the match! Better luck next time. "
+            print(f"\n{Enemy.opp_stats['Profile']} has won the match! Better luck next time. "
                   f"Remember...eye on the prize.")
 
     def trophies(self):
@@ -84,3 +130,4 @@ class Round(CharStats, OppStats):
         else:
             print(f"\nDespite the crushing defeat, {Fighter.char_stats['Profile']} still makes it to the "
                   f"Honorable Mentions list.")
+
