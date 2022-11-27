13cb1b08214fa39c0198c689ae00fc46d00faebc
Wenxi Chu <82306115+wenxichu@users.noreply.github.com>
2022-11-27 13:32:51 -0500

    Secret Characters Added

--- Moves.py
+++ Moves.py
@@ -1,3 +1,4 @@
+import math
 import random
 from Profiles import Fighter, Enemy
 
@@ -33,6 +34,8 @@ class CharSpecial(Fighter, Enemy):
             self.chad()
         elif move == "Feels Bad Man":
             self.pepe()
+        elif move == "Avoid Eye Contact":
+            self.virgin()
 
     def wojak(self):
         amount = int(input("How much charge to spend? (1-3) "))
@@ -49,13 +52,13 @@ class CharSpecial(Fighter, Enemy):
         CharStats.char_hp += 2
         self.char_stats["Attack"] += 1
         OppStats.opp_cg = 0
-        print(f"Doomer\'s attack is increased. {self.opponent_stats['Profile']} loses the will to fight.")
+        print(f"Doomer\'s attack is increased. {self.opp_stats['Profile']} loses the will to fight.")
 
     def zoomer(self):
         CharStats.char_hp += 1
         self.char_stats["Defense"] = 4
-        self.opponent_stats["Attack"] -= 1
-        print(f"Zoomer\'s defense is increased. {self.opponent_stats['Profile']}\'s attacks are now weaker!")
+        self.opp_stats["Attack"] -= 1
+        print(f"Zoomer\'s defense is increased. {self.opp_stats['Profile']}\'s attacks are now weaker!")
 
     def chad(self):
         roll = [1, 2, 3, 4]
@@ -65,13 +68,20 @@ class CharSpecial(Fighter, Enemy):
             return None
         else:
             OppStats.disabled = True
-            print(f"Your knockout punch failed...{self.opponent_stats['Profile']} is crippled this turn.")
+            print(f"Your knockout punch failed...{self.opp_stats['Profile']} is crippled this turn.")
 
     def pepe(self):
         self.char_stats["Defense"] = 3
-        self.opponent_stats["Attack"] -= 1
-        self.opponent_stats["Defense"] -= 1
-        print("{} attack and defense lowered. REEEEE!".format(self.opponent_stats['Profile']))
+        self.opp_stats["Attack"] -= 1
+        self.opp_stats["Defense"] -= 1
+        print("{} attack and defense lowered. REEEEE!".format(self.opp_stats['Profile']))
+
+    def virgin(self):
+        self.char_stats["Attack"] = 3
+        for count in range(2):
+            hp_decay = 0.1 * OppStats.opp_hp
+            OppStats.opp_hp -= math.ceil(hp_decay)
+        print("You ignore the incel hoping he would go away. The music in your ears blocks out everything else.")
 
 
 class OppSpecial(Fighter, Enemy):
@@ -91,6 +101,8 @@ class OppSpecial(Fighter, Enemy):
             self.soyjak()
         elif move == "Tips Fedora":
             self.neckbeard()
+        elif move == "Rant About Society":
+            self.incel()
 
     def mindless_npc(self):
         self.char_stats["Attack"] = 1
@@ -99,24 +111,32 @@ class OppSpecial(Fighter, Enemy):
 
     def meme_man(self):
         self.char_stats["Defense"] -= 2
-        self.opponent_stats["Attack"] += 1
+        self.opp_stats["Attack"] += 1
         print(f"\nUh oh, {self.char_stats['Profile']}\'s defense is weakened from the impending explosion!")
 
     def boomer(self):
         CharStats.char_cg = 0
-        self.opponent_stats["Defense"] -= 1
-        self.opponent_stats["Attack"] += 1
+        self.opp_stats["Defense"] -= 1
+        self.opp_stats["Attack"] += 1
         print(f"\n{self.char_stats['Profile']} is lectured by the boomer. Charge depleted to 0!")
 
     def soyjak(self):
-        self.opponent_stats["Attack"] += 2
+        self.opp_stats["Attack"] += 2
         self.char_stats["Attack"] -= 1
         print("\nSoyjak boosts his attack! {}\'s attack is lower.".format(self.char_stats['Profile']))
 
     def neckbeard(self):
-        self.opponent_stats["Defense"] += 1
+        self.opp_stats["Defense"] += 1
         CharStats.disabled = True
         print(f"\nWhat an awkward greeting...{self.char_stats['Profile']} special attack disabled!")
+
+    def incel(self):
+        coin_flip = ["Heads", "Tails"]
+        if random.choice(coin_flip) == "Heads":
+            self.opp_stats["Attack"] = 6
+        elif coin_flip == "Tails":
+            self.opp_stats["Defense"] = 6
+        print("The incel goes on to blame women for his problems. You give up trying to reason with him.")
 
 
 class Effects:
@@ -127,7 +147,7 @@ class Effects:
         char_copy = Fighter.char_copy
         opp_copy = Enemy.opp_copy
         Fighter.char_stats.update(char_copy)
-        Enemy.opponent_stats.update(opp_copy)
+        Enemy.opp_stats.update(opp_copy)
         CharStats.active, OppStats.active = (0, 0)
 
     @classmethod
@@ -147,3 +167,4 @@ class Effects:
             CharStats.disabled = False
             Effects.reset_stats()
             cls.opp_duration = 3
+
